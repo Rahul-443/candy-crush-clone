6a58b00fb3f55b6f6f87a2f20bf3b95aeaac4d0a
rahul369k <rg085425@gmail.com>
2021-12-03 15:01:41 +0530

    bug fixes

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.use(function(req, res, next) {
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
+    'Accept,Origin,Content-Type,X-LS-CORS-Template,X-LS-Auth-Token,X-LS-Auth-User-Token,Content-Type,X-LS-Sync-Result,X-LS-Sync-Token,X-Requested-With,content-type'
   );
-  res.setHeader('Access-Control-Allow-Credentials', true);
 
   next();
 });
